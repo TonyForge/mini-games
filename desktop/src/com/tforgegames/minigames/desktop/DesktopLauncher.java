61296ad9fe7364bda8e4250b61df0ac6549d83a6
TonyForge <tonyforge.mail@gmail.com>
2016-01-04 14:30:59 +0300

    Master: mini-games

---
+++ desktop/src/com/tforgegames/minigames/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tforgegames.minigames.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tforgegames.minigames.MiniGamesRoot;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MiniGamesRoot(), config);
+	}
+}
